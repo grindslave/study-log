e1ac20251f9f1f164f29b1056afb51c398350792
grindslave <blickwinkel@disroot.org>
2024-09-07 09:00:01 +0800

    Quartz sync: Sep 7, 2024, 9:00 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Study Log✍🏻",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,8 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+        Notes by <a href="https://discordapp.com/users/417843328210436097">gigi</a> created with <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
