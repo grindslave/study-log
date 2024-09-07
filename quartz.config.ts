2495bf819a5202a3cae1181a5c1f562a84b56a72
grindslave <blickwinkel@disroot.org>
2024-09-07 11:50:16 +0800

    Quartz sync: Sep 7, 2024, 11:50 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "STUDY LOG ✍🏻",
+    pageTitle: "Study Log✍🏻",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
